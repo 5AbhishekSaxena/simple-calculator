5f9b82a4dadf2f8cb3edb917593fc89def1b3f1c
Aditya Saini <72370777+Adityasaini10012001@users.noreply.github.com>
2023-03-01 11:15:51 +0530

    Add division operation

--- src/main/java/org/example/service/Calculator.java
+++ src/main/java/org/example/service/Calculator.java
@@ -12,5 +12,11 @@ public class Calculator {
     public int multiply(int num1, int num2) {
         return num1 * num2;
     }
+    public int division(int num1, int num2) {
+        if( num2 == 0 )
+            return 0;
+        else
+            return num1/num2;
+    }
     
 }
