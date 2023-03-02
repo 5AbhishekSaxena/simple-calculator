02a76f98d762afe88a3b835a8ec952a5c28ebb16
nitishkumarmaurya <nitishkumarmaurya820.hitit2020@gmail.com>
2023-03-02 08:55:45 +0530

    added new functionalities

--- src/main/java/org/example/service/Calculator.java
+++ src/main/java/org/example/service/Calculator.java
@@ -9,4 +9,21 @@ public class Calculator {
     public int subtract(int num1, int num2) {
         return num1 - num2;
     }
+
+    public int multiply(int num1,int num2){
+        return num1*num2;
+    }
+
+    public int divide(int num1,int num2)
+    {
+        float num=num2;
+        if(num2!=0)
+        {
+            return num1/num;
+        }
+        else{
+            System.out.println("denominator can't be zero");
+            return 0;
+        }
+    }
 }
